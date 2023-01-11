3ec8a078d23593c0b5f1cad098d5a98a23fba24d
Jonathan-Evey <jonathan.c.evey@gmail.com>
2023-01-11 13:06:26 -0800

    files renamed

--- routes/store.js
+++ routes/store.js
@@ -9,7 +9,7 @@ const items_controller = require('../controllers/itemController');
 // ------ index
 router.get('/', function (req, res, next) {
 	console.log('index');
-	res.render('store/index', { title: 'Store' });
+	res.render('store/index', { title: 'Store', at_page: 'store' });
 });
 
 ///  CATEGORY ROUTES ///
@@ -23,5 +23,7 @@ module.exports = router;
 
 // ------ index
 router.get('/full-catalog', items_controller.item_list);
+
+router.get('/full-catalog/:id', items_controller.item_details);
 
 module.exports = router;
