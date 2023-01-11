9c3044b109139622fb9f6f65b9935be543e4ce8c
NighIsTheEnd <jonathan.c.evey@gmail.com>
2023-01-11 07:17:50 -0800

    add starter files for about and contact pages

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-	console.log('index');
-	res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  console.log("index");
+  res.render("index", { title: "Express" });
+});
+
+router.get("/about", function (req, res, next) {
+  res.render("about", { title: "About me" });
+});
+
+router.get("/contact", function (req, res, next) {
+  res.render("contact", { title: "Contact" });
 });
 
 module.exports = router;
