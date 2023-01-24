8a43794723d17d82b324b471ce2c27c542e1d2fb
NighIsTheEnd <jonathan.c.evey@gmail.com>
2023-01-24 09:17:25 -0800

    minor code format update

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,36 +1,37 @@
-const Item = require('../models/item');
+const Item = require("../models/item");
 
 // show all items
 exports.item_list = function (req, res, next) {
-	Item.find()
-		.sort([['name', 'ascending']])
-		.exec(function (err, response) {
-			if (err) {
-				return next(err);
-			}
-			res.render('store/items/index', {
-				title: 'All Items',
-				all_items: response,
-				at_page: 'store',
-			});
-		});
+  Item.find()
+    .sort([["name", "ascending"]])
+    .exec(function (err, response) {
+      if (err) {
+        return next(err);
+      }
+      res.render("store/items/index", {
+        title: "All Items",
+        all_items: response,
+        at_page: "store",
+      });
+    });
 };
 
 // display one item's details
 exports.item_details = function (req, res, next) {
-	let itemName = req.params.id.replaceAll('-', ' ');
-	console.log(itemName);
-	Item.findOne({ name: { $regex: itemName, $options: 'i' } }).exec(
-		function (err, response) {
-			if (err) {
-				return next(err);
-			}
-			console.log(response);
-			res.render('store/items/item', {
-				title: response.name,
-				itemObj: response,
-				at_page: 'store',
-			});
-		}
-	);
+  let itemName = req.params.id.replaceAll("-", " ");
+  console.log(itemName);
+  Item.findOne({ name: { $regex: itemName, $options: "i" } }).exec(function (
+    err,
+    response
+  ) {
+    if (err) {
+      return next(err);
+    }
+    console.log(response);
+    res.render("store/items/item", {
+      title: response.name,
+      itemObj: response,
+      at_page: "store",
+    });
+  });
 };
--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,35 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const index_controller = require('../controllers/indexController');
+const index_controller = require("../controllers/indexController");
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-	console.log('index');
-	res.render('index', { title: 'Express', at_page: 'home' });
+router.get("/", function (req, res, next) {
+  console.log("index");
+  res.render("index", { title: "Art by Lynn", at_page: "home" });
 });
 
-router.get('/gallery', function (req, res, next) {
-	res.render('gallery/index', {
-		title: 'Gallery',
-		at_page: 'gallery',
-	});
+router.get("/gallery", function (req, res, next) {
+  res.render("gallery/index", {
+    title: "Gallery",
+    at_page: "gallery",
+  });
 });
 
-router.get('/about', function (req, res, next) {
-	res.render('about/index', {
-		title: 'About me',
-		at_page: 'about',
-	});
+router.get("/about", function (req, res, next) {
+  res.render("about/index", {
+    title: "About me",
+    at_page: "about",
+  });
 });
 
-router.get('/contact', function (req, res, next) {
-	res.render('contact/index', {
-		title: 'Contact',
-		at_page: 'contact',
-	});
+router.get("/contact", function (req, res, next) {
+  res.render("contact/index", {
+    title: "Contact",
+    at_page: "contact",
+  });
 });
 
-router.post('/contact', index_controller.contact_form_post);
+router.post("/contact", index_controller.contact_form_post);
 
 module.exports = router;
