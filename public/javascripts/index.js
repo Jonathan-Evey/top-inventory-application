3f3acb47c235a9604ddab24b8d381d371c9f8540
Jonathan-Evey <jonathan.c.evey@gmail.com>
2023-07-05 08:02:16 -0700

    component and styles for back to top button

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,3 +3,4 @@ import mobileNav from './mobileMainNavEvents.js';
 import mobileFilter from './mobileGalleryNavEvents.js';
 import contactEvents from './contactEvents.js';
 import galleryImgs from './galleryImgLoadEvents.js';
+import toTopBtn from './galleryToTopBtn.js';
